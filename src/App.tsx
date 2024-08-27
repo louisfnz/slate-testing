5f0411a8db6b80d9650a586807b876bda930cf31
louisfnz <25600218+louisfnz@users.noreply.github.com>
2024-08-27 14:43:59 +1200

    wip

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import Content from "./Content";
+import SlateTables from "./SlateTables";
+
+
+const App = () => {
+  return (
+    <div style={{margin:'50px auto',maxWidth: '800px',backgroundColor:'white'}}>
+      {/*<div contentEditable>*/}
+      {/*  <p>*/}
+      {/*    asd asd asd*/}
+      {/*    as d*/}
+      {/*  </p>*/}
+      {/*  <table>*/}
+      {/*    <tr>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*    </tr>*/}
+      {/*    <tr>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*    </tr>*/}
+      {/*    <tr>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*      <td>cell asd asd s ad</td>*/}
+      {/*    </tr>*/}
+      {/*  </table>*/}
+      {/*  <p>*/}
+      {/*    asd asd asd*/}
+      {/*    as d*/}
+      {/*  </p>*/}
+
+      {/*</div>*/}
+      <SlateTables/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Content.tsx
@@ -0,0 +1,196 @@
+import React, {CSSProperties, useCallback, useMemo} from 'react'
+import { Slate, Editable, withReact } from 'slate-react'
+import {
+  Editor,
+  Range,
+  Point,
+  Descendant,
+  createEditor,
+  Element as SlateElement,
+} from 'slate'
+import { withHistory } from 'slate-history'
+
+const TablesExample = () => {
+  const renderElement = useCallback(props => <Element {...props} />, [])
+  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+  const editor = useMemo(
+    () => withTables(withHistory(withReact(createEditor()))),
+    []
+  )
+  return (
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
+    </Slate>
+  )
+}
+
+const withTables = editor => {
+  const { deleteBackward, deleteForward, insertBreak } = editor
+
+  editor.deleteBackward = unit => {
+    const { selection } = editor
+
+    if (selection && Range.isCollapsed(selection)) {
+      const [cell] = Editor.nodes(editor, {
+        match: n =>
+          !Editor.isEditor(n) &&
+          SlateElement.isElement(n) &&
+          n.type === 'table-cell',
+      })
+
+      if (cell) {
+        const [, cellPath] = cell
+        const start = Editor.start(editor, cellPath)
+
+        if (Point.equals(selection.anchor, start)) {
+          return
+        }
+      }
+    }
+
+    deleteBackward(unit)
+  }
+
+  editor.deleteForward = unit => {
+    const { selection } = editor
+
+    if (selection && Range.isCollapsed(selection)) {
+      const [cell] = Editor.nodes(editor, {
+        match: n =>
+          !Editor.isEditor(n) &&
+          SlateElement.isElement(n) &&
+          n.type === 'table-cell',
+      })
+
+      if (cell) {
+        const [, cellPath] = cell
+        const end = Editor.end(editor, cellPath)
+
+        if (Point.equals(selection.anchor, end)) {
+          return
+        }
+      }
+    }
+
+    deleteForward(unit)
+  }
+
+  editor.insertBreak = () => {
+    const { selection } = editor
+
+    if (selection) {
+      const [table] = Editor.nodes(editor, {
+        match: n =>
+          !Editor.isEditor(n) &&
+          SlateElement.isElement(n) &&
+          n.type === 'table',
+      })
+
+      if (table) {
+        return
+      }
+    }
+
+    insertBreak()
+  }
+
+  return editor
+}
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case 'table': {
+      const style: CSSProperties = {
+        width:'100%',
+        borderCollapse: 'collapse',
+        whiteSpace: 'pre-wrap',
+        overflowWrap: 'break-word',
+      }
+      return (
+        <table style={style}>
+          <tbody {...attributes}>{children}</tbody>
+        </table>
+      )
+    }
+    case 'table-row':
+      return <tr {...attributes}>{children}</tr>
+    case 'table-cell': {
+      const style: CSSProperties = {
+        border:'1px solid #999',
+        padding:'0.5em'
+      }
+      return <td {...attributes} style={style}>{children}</td>
+    }
+    default:
+      return <p {...attributes}>{children}</p>
+  }
+}
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+
+  return <span {...attributes}>{children}</span>
+}
+
+const cellContent = () => {
+  return {
+    type: 'table-cell',
+    // children: [{type: 'paragraph', children:[{ text: '' }]}],
+    children:[{ text: 'asd asd asd asd ' }]
+  };
+}
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: 'Since the editor is based on a recursive tree model, similar to an HTML document, you can create complex nested structures, like tables:',
+      },
+    ],
+  },
+  {
+    type: 'table',
+    children: [
+      {
+        type: 'table-row',
+        children: [
+          cellContent(),
+          cellContent(),
+          cellContent(),
+          cellContent()
+        ],
+      },
+      {
+        type: 'table-row',
+        children: [
+          cellContent(),
+          cellContent(),
+          cellContent(),
+          cellContent()
+        ],
+      },
+      {
+        type: 'table-row',
+        children: [
+          cellContent(),
+          cellContent(),
+          cellContent(),
+          cellContent()
+        ],
+      },
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: "This table is just a basic example of rendering a table, and it doesn't have fancy functionality. But you could augment it to add support for navigating with arrow keys, displaying table headers, adding column and rows, or even formulas if you wanted to get really crazy!",
+      },
+    ],
+  },
+]
+
+export default TablesExample
